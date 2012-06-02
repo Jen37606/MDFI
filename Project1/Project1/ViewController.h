5a243ecbe2a4af7b1cb9281dbd0598999b5b0a65
Jen <jen37606@yahoo.com>
2012-06-01 20:02:29 -0400

    Started Project 1

---
+++ Project1/Project1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Project1
+//
+//  Created by Jennifer Johnson on 6/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
